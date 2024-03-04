1953975daae600d4e62881966c04e695bf796328
ItzMeWoltik <itzmewoltik@gmail.com>
2024-03-05 00:33:49 +0200

    ви дуже комітів просили, от вони

---
+++ perev_oper/MyPoint.cs
@@ -0,0 +1,28 @@
+﻿namespace perev_oper;
+
+public class MyPoint
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public MyPoint(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public static MyPoint operator +(MyPoint point1, MyPoint point2)
+    {
+        return new MyPoint(point1.X + point2.X, point1.Y + point2.Y);
+    }
+
+    public static MyPoint operator -(MyPoint point1, MyPoint point2)
+    {
+        return new MyPoint(point1.X - point2.X, point1.Y - point2.Y);
+    }
+
+    public override string ToString()
+    {
+        return $"({X}, {Y})";
+    }
+}
---
+++ perev_oper/Program.cs
@@ -0,0 +1,21 @@
+﻿using perev_oper;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyPoint point1 = new MyPoint(10, 20);
+        MyPoint point2 = new MyPoint(5, 10);
+
+        MyPoint sum = point1 + point2;
+        MyPoint difference = point1 - point2;
+
+        Console.WriteLine("Додавання:");
+        Console.WriteLine($"{point1} + {point2} = {sum}");
+
+        Console.WriteLine("\nВіднімання:");
+        Console.WriteLine($"{point1} - {point2} = {difference}");
+
+        Console.ReadLine();
+    }
+}
